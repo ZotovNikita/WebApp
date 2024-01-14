57038c466b93e1b6eec4ab6bba803d293bfad98e
ZotovNikita <usernamex821@gmail.com>
2024-01-15 01:22:36 +0600

    add header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { Link } from "react-router-dom";
+import headerStyles from './Header.module.css';
+
+function Header() {
+    const [activeLink, setActiveLink] = useState('');
+
+    const handleNavLinkClick = (path: string) => {
+        setActiveLink(path);
+    };
+
+    return (
+        <header className={headerStyles.headerBar}>
+            <div className={headerStyles.brandLogo}>
+                <img src="src/assets/images/header/logo.png" alt="HealthCareLogo" className={headerStyles.logoImage}/>
+                HealthCare
+            </div>
+            <nav className={headerStyles.navigation}>
+                <Link to="/" className={`${headerStyles.navLink} ${activeLink === '/' ? headerStyles.activeLink : ''}`} onClick={() => handleNavLinkClick('/')}>Home</Link>
+                <Link to="/find-doctor" className={`${headerStyles.navLink} ${activeLink === '/find-doctor' ? headerStyles.activeLink : ''}`} onClick={() => handleNavLinkClick('/find-doctor')}>Find a doctor</Link>
+                <Link to="/apps" className={`${headerStyles.navLink} ${activeLink === '/apps' ? headerStyles.activeLink : ''}`} onClick={() => handleNavLinkClick('/apps')}>Apps</Link>
+                <Link to="/testimonials" className={`${headerStyles.navLink} ${activeLink === '/testimonials' ? headerStyles.activeLink : ''}`} onClick={() => handleNavLinkClick('/testimonials')}>Testimonials</Link>
+                <Link to="/about" className={`${headerStyles.navLink} ${activeLink === '/about' ? headerStyles.activeLink : ''}`} onClick={() => handleNavLinkClick('/about')}>About us</Link>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/fake_data/Announcement.tsx
@@ -0,0 +1,9 @@
+function Anons() {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '77vh' }}>
+        <h1>This page will be available later &#40;&#62;&#969;&#94;&#41;</h1>
+      </div>
+    );
+  }
+  
+  export default Anons;
