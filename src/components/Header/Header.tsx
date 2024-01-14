25f197a5460edcf53ac88af1ac1a208263685783
ZotovNikita <usernamex821@gmail.com>
2024-01-15 05:45:51 +0600

    add service cards

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Link } from "react-router-dom";
 import headerStyles from './Header.module.css';
+import { appStrings } from '../../content/strings';
 
 function Header() {
     const [activeLink, setActiveLink] = useState('');
@@ -12,7 +13,7 @@ function Header() {
     return (
         <header className={headerStyles.headerBar}>
             <div className={headerStyles.brandLogo}>
-                <img src="src/assets/images/header/logo.png" alt="HealthCareLogo" className={headerStyles.logoImage}/>
+                <img src={appStrings.logo} alt="HealthCareLogo" className={headerStyles.logoImage}/>
                 HealthCare
             </div>
             <nav className={headerStyles.navigation}>
